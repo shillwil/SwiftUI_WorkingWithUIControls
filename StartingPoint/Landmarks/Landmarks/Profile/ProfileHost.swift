858f8e7786cc9c8bb34f336a616b00ad9b02d951
Alex Shillingford <alex.shillingford@icloud.com>
2020-01-28 16:11:19 -0700

    Display a User Profile

---
+++ StartingPoint/Landmarks/Landmarks/Profile/ProfileHost.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Alex Shillingford on 1/28/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
